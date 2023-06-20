4fb97925a2f205028028a39bd061d467f5c2dda8
moodfactor <moodfactor@hotmail.com>
2023-06-20 21:06:56 +0300

    link between a widget with id and a function.

--- NewsApp.py
+++ NewsApp.py
@@ -1,10 +1,18 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ObjectProperty
+
 
 class NewsHome(BoxLayout):
-    label_text  = StringProperty("Hi")
     
+    label_text  = StringProperty("Hi")
+    label_1 = ObjectProperty()
+
+    def change_label(self, value) -> str:
+       self.ids.my_button.text = value
+       
+       return value
+
     
 class NewsApp(App):
     def build(self):
