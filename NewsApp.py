a6bd17b50ac5da0f33ae6765c4f1c4bc4411c484
moodfactor <moodfactor@hotmail.com>
2023-06-21 12:20:50 +0300

    implement button with the call back

--- NewsApp.py
+++ NewsApp.py
@@ -3,21 +3,34 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty, ObjectProperty
 from kivy.clock import Clock
 from kivy.uix import widget
+from kivy.uix import button
 
 
 class NewsHome(BoxLayout):
-    
-    label_text  = StringProperty("Hi")
+
+    label_text = StringProperty("Hi")
     label_1 = ObjectProperty()
 
-    def change_label(self, value) -> str:
-       self.ids.my_button.text = value
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
        
-       return value
-   
+
+    def state_callback(self, obj, value):        
+        print("State changed for ", obj, "to ", value)
+        super().state_callback(obj, value)
+
+    def on_press_callback(self) -> str:
+        # self.ids.label_test2.text = "press on button"
+        print("Booooooom")
+
+    def change_label(self, value) -> str:
+        self.ids.my_button.text = value
+
+        return value
+
     def my_callback(self, dt):
         self.ids.label_test.text = "My callback is called! " + str(dt)
-        
+
     def on_touch_down(self, touch):
         Clock.schedule_interval(self.my_callback, 1/30)
         if touch.is_double_tap:
@@ -25,7 +38,7 @@ class NewsHome(BoxLayout):
             Clock.unschedule(self.my_callback)
         return super().on_touch_down(touch)
 
-    
+
 class NewsApp(App):
     def build(self):
         return NewsHome()
