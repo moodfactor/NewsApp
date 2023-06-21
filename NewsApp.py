aa77ed19c997960be3b1a8cbb6de80cea9732eb9
moodfactor <moodfactor@hotmail.com>
2023-06-21 09:50:48 +0300

    schedule an event to be triggered after 1s of mouse click

--- NewsApp.py
+++ NewsApp.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty, ObjectProperty
+from kivy.clock import Clock
+from kivy.uix import widget
 
 
 class NewsHome(BoxLayout):
@@ -12,6 +14,13 @@ class NewsHome(BoxLayout):
        self.ids.my_button.text = value
        
        return value
+   
+    def my_callback(self, dt):
+        self.ids.label_test.text = "My callback is called! " + str(dt)
+        
+    def on_touch_down(self, touch):
+        Clock.schedule_once(self.my_callback, 1)
+        return super().on_touch_down(touch)
 
     
 class NewsApp(App):
