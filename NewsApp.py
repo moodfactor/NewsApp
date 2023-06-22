5626a4c39463dd5b961cef1bc3e09a0962920610
moodfactor <moodfactor@hotmail.com>
2023-06-22 10:11:55 +0300

    convert layout to Grid layout inside a Box layout to indent well

--- NewsApp.py
+++ NewsApp.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.properties import StringProperty, ObjectProperty
 from kivy.clock import Clock
 from kivy.uix.widget import Widget
