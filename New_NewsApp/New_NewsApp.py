def2c095e2c5248b9e0369dcbf9fb2e665f0443a
moodfactor <moodfactor@hotmail.com>
2023-06-25 22:07:55 +0300

    define mutliple screens

--- New_NewsApp/New_NewsApp.py
+++ New_NewsApp/New_NewsApp.py
@@ -1,7 +1,11 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen, CardTransition, FadeTransition
+from kivy.uix.screenmanager import WipeTransition, Screen, CardTransition, FadeTransition
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+
 
 # Create both screens. Please note the root.manager.current: this is how
 # you can control the ScreenManager from kv. Each screen has by default a
@@ -10,61 +14,21 @@ from kivy.uix.floatlayout import FloatLayout
 # Define a string variable to store the kv code
 kv = """
 <MyW>:
-    # Use a FloatLayout as the root widget
-    FloatLayout:
-        # Add an ActionBar widget as a child of the FloatLayout
-        ActionBar:
-            pos_hint: {'top': 1}
-            ActionView:
-                use_separator: True
-                
-                ActionPrevious:
-                    title: 'News'
-                    with_previous: False
-                
-                ActionButton:
-                    text: 'Btn0'
-                    icon: 'floppy.png'
-                
-                ActionButton:
-                    text: 'Btn1'
-                
-                ActionButton:
-                    text: 'Btn2'
-                
-                ActionButton:
-                    text: 'Btn3'
-                
-                ActionButton:
-                    text: 'Btn4'
-                
-                ActionGroup:
-                    text: 'Group'
-           
-                    ActionButton:
-                        text: 'Btn5'
-           
-                    ActionButton:
-                        text: 'Btn6'
-           
-                    ActionButton:
-                        text: 'Btn7'
-
-<MyW1>:
-    # Use a FloatLayout as the root widget
-    FloatLayout:
-        # Add two Button widgets as children of the FloatLayout
-        Button:
-            id: label1
-            size_hint: .2, .2
-            pos_hint: {'center_x': .5, 'center_y': .5}
-            text: 'B1'
+    BoxLayout: 
+        size_hint_y: None
+        height: 30
+        id: buttons
+    
+    # Create a screen manager to switch between screens
+    ScreenManager:
+    # Give an id to the screen manager for easy reference
+        id: sm
+ 
         
-        Button:
-            id: label2
-            size_hint: .1, .1
-            pos_hint: {'center_x': .1, 'center_y': .1}
-            text: 'B2'
+
+<MButton>:
+    on_press: app.root.ids.sm.current = self.text
+        
             
 """
 
@@ -73,17 +37,46 @@ kv = """
 Builder.load_string(kv)
 
 # Define a class for the first screen that inherits from FloatLayout
-class MyW(FloatLayout):
+
+
+class MyW(BoxLayout):
     def __init__(self, **kwargs):
         super(MyW, self).__init__(**kwargs)
-        self.add_widget(MyW1())
-        
+
+        # Create a screen with the name "Hello1"
+        s = Screen(name="Hello1")
+        # Add a label with the text "src1" to the screen
+        s.add_widget(Label(text="src1"))
+        # Add the screen to the screen manager
+        self.ids.sm.add_widget(s)
+        # Set the transition type for the screen manager to WipeTransition
+        self.ids.sm.transition = FadeTransition()
+
+        # Create another screen with the name "Hello2"
+        s = Screen(name="Hello2")
+        # Add a label with the text "src2" to the screen
+        s.add_widget(Label(text="src2"))
+        # Add the screen to the screen manager
+        self.ids.sm.add_widget(s)
+        # Set the transition type for the screen manager to WipeTransition
+        self.ids.sm.transition = WipeTransition()
+
+        # Create a button with the text "Hello1"
+        self.ids.buttons.add_widget(MButton(
+        text="Hello1"))
+        # Create another button with the text "Hello2"
+        self.ids.buttons.add_widget(MButton(
+        text="Hello2"))
+
+
 
 # Define a class for the second screen that inherits from FloatLayout
-class MyW1(FloatLayout):
+class MButton(Button):
     pass
 
 # Define a class for the app that inherits from App
+
+
 class New_NewsApp(App):
     def build(self):
         return MyW()
