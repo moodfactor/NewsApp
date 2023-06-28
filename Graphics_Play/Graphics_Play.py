17ae9f273ea523ac10bfd63343dbc92973ac842a
moodfactor <moodfactor@hotmail.com>
2023-06-28 13:17:32 +0300

    create a custom rule to provide custom graphics to another layout

--- Graphics_Play/Graphics_Play.py
+++ Graphics_Play/Graphics_Play.py
@@ -8,29 +8,57 @@ from kivy.factory import Factory
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.atlas import Atlas
+from kivy.uix.floatlayout import FloatLayout
 
-
+# define a string variable that contains the kv language code for the UI design
 kv = """
 
+<CustomLayout>:
+    # define a canvas instruction that draws a yellow rectangle before the widget content
+    canvas.before:
+        Color:
+            # set the color to yellow (red, green, blue, alpha)
+            rgba: 1, 1, 0, 1
+        Rectangle: 
+            # set the position of the rectangle to the widget
+            pos: self.pos
+            # set the size of the rectangle to the widget size
+            size: self.size
+
+ # define a rule for MyW widget, which is a subclass of BoxLayout
+<MyW>:
+    CustomLayout:
+    
+        # add an AsyncImage widget as a child of CustomLayout widget,
+        # which loads an image asynchronously from a source
+        AsyncImage:
+            source: '11.png'
+            size_hint: 1, .5
+            pos_hint: {'center_x': .5, 'center_y': .5}
+            
+    AsyncImage:
+        source: '22.png'
+
+    # add another CustomLayout widget as a child of MyW widget
+    CustomLayout:
+        AsyncImage:
+            source: '33.png'
+            size_hint: 1, .5
+            pos_hint: {'center_x': .5, 'center_y': .5}
     
     
 """
 
+# load the kv string into memory using Builder class 
 Builder.load_string(kv)
 
 
 class MyW(BoxLayout):
-    def __init__(self, **kwargs):
-        super(MyW, self).__init__(**kwargs)
+    pass
 
-        # To create atlas do in terminal: python -m kivy.atlas myatlas 642 *.png
-        # myatlas is the name of the atlas
-        # 642 is the size of the images
-        # *.png is the file type of the images
 
-        btn = Button(text='', size=(500, 500), background_normal='atlas://myatlas/11',
-                     background_down='atlas://myatlas/33', size_hint=(None, None))
-        self.add_widget(btn)
+class CustomLayout(FloatLayout):
+    pass
 
 
 class MyApp(App):
