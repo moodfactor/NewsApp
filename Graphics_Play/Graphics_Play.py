d0e55e322b5f5c4326e30f8d9a6d00451fcb1d5c
moodfactor <moodfactor@hotmail.com>
2023-06-29 11:11:21 +0300

    Created a custom widget

--- Graphics_Play/Graphics_Play.py
+++ Graphics_Play/Graphics_Play.py
@@ -9,52 +9,31 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.atlas import Atlas
 from kivy.uix.floatlayout import FloatLayout
+from kivy.graphics import Color, Ellipse, Line
+
 
 # define a string variable that contains the kv language code for the UI design
 kv = """
 
-<CustomLayout>:
-    # define a canvas instruction that draws a yellow rectangle before the widget content
-    canvas.before:
-        Color:
-            # set the color to yellow (red, green, blue, alpha)
-            rgba: 1, 1, 0, 1
-        Rectangle: 
-            # set the position of the rectangle to the widget
-            pos: self.pos
-            # set the size of the rectangle to the widget size
-            size: self.size
 
- # define a rule for MyW widget, which is a subclass of BoxLayout
-<MyW>:
-    CustomLayout:
-    
-        # add an AsyncImage widget as a child of CustomLayout widget,
-        # which loads an image asynchronously from a source
-        AsyncImage:
-            source: '11.png'
-            size_hint: 1, .5
-            pos_hint: {'center_x': .5, 'center_y': .5}
-            
-    AsyncImage:
-        source: '22.png'
-
-    # add another CustomLayout widget as a child of MyW widget
-    CustomLayout:
-        AsyncImage:
-            source: '33.png'
-            size_hint: 1, .5
-            pos_hint: {'center_x': .5, 'center_y': .5}
     
     
 """
 
-# load the kv string into memory using Builder class 
+# load the kv string into memory using Builder class
 Builder.load_string(kv)
 
 
-class MyW(BoxLayout):
-    pass
+class MyW(Widget):
+    def on_touch_down(self, touch):
+        with self.canvas:
+            Color(1, 1, 0)
+            d = 30.
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            touch.ud['line'] = Line(points=(touch.x, touch.y))
+
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
 
 
 class CustomLayout(FloatLayout):
