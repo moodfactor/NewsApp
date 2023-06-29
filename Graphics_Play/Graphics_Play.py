a52737c56f2bac60f9e26ded32b51afe952dd4fa
moodfactor <moodfactor@hotmail.com>
2023-06-29 18:52:10 +0300

    load and show image from web

--- Graphics_Play/Graphics_Play.py
+++ Graphics_Play/Graphics_Play.py
@@ -1,15 +1,17 @@
-import cProfile
-from kivy.graphics import Color, Ellipse, Line
-from kivy.uix.floatlayout import FloatLayout
-from kivy.atlas import Atlas
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.factory import Factory
-from kivy.uix.carousel import Carousel
-from kivy.lang import Builder
-from kivy.uix.widget import Widget
-from kivy.uix.label import Label
+from kivy.loader import Loader
+from kivy.uix.image import AsyncImage
 from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.carousel import Carousel
+from kivy.factory import Factory
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.atlas import Atlas
+from kivy.uix.floatlayout import FloatLayout
+from kivy.graphics import Color, Ellipse, Line, Rectangle
+import cProfile
 import kivy
 kivy.require('1.9.0')
 
@@ -17,6 +19,9 @@ kivy.require('1.9.0')
 # define a string variable that contains the kv language code for the UI design
 kv = """
 
+<Myw>:
+
+ 
 
     
     
@@ -26,17 +31,8 @@ kv = """
 Builder.load_string(kv)
 
 
-class MyW(Widget):
-
-    def on_touch_down(self, touch):
-        with self.canvas:
-            Color(1, 1, 0)
-            d = 30.
-            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
-            touch.ud['line'] = Line(points=(touch.x, touch.y))
-
-    def on_touch_move(self, touch):
-        touch.ud['line'].points += [touch.x, touch.y]
+class MyW(BoxLayout):
+    pass
 
 
 class CustomLayout(FloatLayout):
@@ -45,19 +41,24 @@ class CustomLayout(FloatLayout):
 
 class MyApp(App):
 
-    def on_start(self):
-        print("on_start")
-        self.profile = cProfile.Profile()
-        self.profile.enable()
+    def _image_loaded(self, proxyImage):
+        if proxyImage.image.texture:
+            self.image.texture = proxyImage.image.texture
 
-    def on_stop(self):
-        print("on_stop")
-        self.profile.disable()
-        s = self.profile.print_stats()
-        # save s to file s.profile on disk
-        self.profile.dump_stats("s.profile")
     def build(self):
-        return MyW()
+        proxyImage = Loader.image(
+            "https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
+        proxyImage.allow_stretch = True
+        proxyImage.keep_ratio = False
+        proxyImage.anim_delay = 0.1
+        proxyImage.anim_loop = True
+        proxyImage.anim_repeat = True
+        proxyImage.loading_image = "R.png"
+        proxyImage.error_image = "R.png"
+        proxyImage.bind(on_load=self._image_loaded)
+        proxyImage.bind(on_error=self._image_loaded)
+        self.image = AsyncImage()
+        return self.image
 
 
 if __name__ == "__main__":
