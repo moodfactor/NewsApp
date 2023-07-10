bc98a1a011fa88dac2ad5532e4bea1c81079b890
moodfactor <moodfactor@hotmail.com>
2023-07-10 13:32:52 +0300

    bug fix and general imporvments

--- NewsApp_3/try.py
+++ NewsApp_3/try.py
@@ -6,43 +6,66 @@ from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.properties import StringProperty
 
 KV = '''
+
+
 <CustomCard>:
-    orientation: "vertical"
-    background_color: 1, 1, .2, .8
-    
-    MDCard:
+    BoxLayout:
         size_hint: None, None
         size: 300, 200
-        pos_hint: {"center_x": 0.5, "center_y": 0.5}
+        pos_hint: {"center_x": 0.4, "center_y": 0.5}
         elevation: 4
         radius: [20,]
-        md_bg_color: 1, 1, 1, 1
-        on_press: print("Press")
+        md_bg_color: .3, .3, 1, 1
+        orientation: "vertical"
+    
+        canvas.before:
+            Color:
+                rgba: .8, .5, .1,  # canvas white color
             
-        BoxLayout:
-            orientation: "vertical"
-            spacing: 10
-            padding: 10
             
-            Image:
-                source: "image.jpeg"
-                size_hint: 1, 1
-                pos_hint: {"center_x": 0.5, "center_y": 0.5}
-                allow_stretch: True
+            Rectangle:
+                pos: self.pos
+                size: 200, 300
+                
+    
+        MDCard:
+            size_hint: None, None
+            size: 300, 200
+            pos_hint: {"center_x": 0.5, "center_y": 0.5}
+            elevation: 4
+            radius: [20,]
+            md_bg_color: 0, 1, 1, 1 # aqua color
+            on_press: print("Press")
             
-            MDLabel:
-                text: "Ahmed"
-                size_hint: 1, 0.2
-                halign: "center"
-                color: (0, 0, 0, 1)
-                font_style: "H6"
-                pos_hint: {"center_x": 0.5, "center_y": 0.5}
+            
+            BoxLayout:
+                orientation: "vertical"
+                
+
+            
+                Image:
+                    source: "image.jpeg"
+                    size_hint: 1, 1
+                    pos_hint: {"center_x": 0.5, "center_y": 0.5}
+                    allow_stretch: True
+            
+                MDLabel:
+                    text: "Ahmed"
+                    size_hint: 1, 0.2
+                    halign: "center"
+                    color: (0, 1, 0, 1)
+                    font_style: "H6"
+                    pos_hint: {"center_x": 0.5, "center_y": 0.5}
 
 RecycleView:
     viewclass: 'CustomCard'
     data: app.data
+    size_hint: 1, 1
+    
     RecycleBoxLayout:
-        elevation: 4
+        elevation: 2
+        spacing: 10
+        padding: 10
         default_size: None, dp(200)
         default_size_hint: 1, None
         size_hint_y: None
