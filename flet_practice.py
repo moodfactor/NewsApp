b4967a507c51837d507acec5a9fa89d4d1dc9749
moodfactor <moodfactor@hotmail.com>
2023-07-27 20:38:30 +0300

    implemented Really Organised Row wit Ref, and dropdown menu

--- flet_practice.py
+++ flet_practice.py
@@ -50,4 +50,33 @@ def main(page: ft.Page):
     new_task = ft.TextField(hint_text= "Whats needs to be done?", width= 300)
     page.add(ft.Row([new_task, ft.ElevatedButton("Add", on_click=add_clicked)]))
 
+    output_text = ft.Ref[ft.Text]()
+    submit_button = ft.Ref[ft.ElevatedButton]()
+    color_dropdown = ft.Ref[ft.Dropdown]()
+    my_row = ft.Ref[ft.Row]()
+    
+    def button_clicked1(e):
+        output_text.current.value = f"Dropdown value is: {color_dropdown.value}"
+        page.update()
+        
+    # page.controls.append(ft.Row(ref=my_row,controls= [
+    #     ft.Dropdown(label="Color", options=["Red", "Green", "Blue"], ref=color_dropdown),
+    #     ft.ElevatedButton(text="Submit", on_click=button_clicked1, ref=submit_button),
+    #     ft.Text(value="",ref=output_text)
+    # ]))
+    
+    page.controls.append(ft.Row(ref=my_row,controls= [
+        ft.Dropdown(width = 100, options=[
+            ft.dropdown.Option("Red"),
+            ft.dropdown.Option("Green"),
+            ft.dropdown.Option("Blue"),], ref=color_dropdown),
+        ft.ElevatedButton(text="Submit", on_click=button_clicked1, ref=submit_button),
+        ft.Text(value="xerAW Mood!", ref=output_text),
+    ]))
+    
+
+    
+    
+    page.update()
+
 ft.app(target=main)
